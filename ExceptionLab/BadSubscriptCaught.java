7a7ca7e29b138ba5096910e5827f15bee09008e2
maxnuss1021 <152890637+maxnuss1021@users.noreply.github.com>
2024-02-27 14:15:27 -0500

    Update BadSubscriptCaught.java

--- ExceptionLab/BadSubscriptCaught.java
+++ ExceptionLab/BadSubscriptCaught.java
@@ -15,18 +15,18 @@ public class BadSubscriptCaught {
         catch(IndexOutOfBoundsException e){
             System.out.println("please re-input an integer between 1 and 10:");
              x = in.nextInt();
-            System.out.println(ar[x]);
+            System.out.println(ar[x-1]);
         }
             //if the input is not an integer, it will prompt the user to input an integer
         catch(InputMismatchException e){
             System.out.println("Please input an INTEGER");
             x = in.nextInt();
-            System.out.println(ar[x]);
+            System.out.println(ar[x-1]);
         }
         catch(Exception e){
             System.out.println("Error, please try again");
             x = in.nextInt();
-            System.out.println(ar[x]);
+            System.out.println(ar[x-1]);
         }
     }
 }
