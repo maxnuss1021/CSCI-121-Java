c4b571b003c2bc5423a4ccb500df64be36e647c9
maxnuss1021 <152890637+maxnuss1021@users.noreply.github.com>
2024-02-27 18:49:17 +0000

    Main

---
+++ ExceptionLab/BadSubscriptCaught.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class BadSubscriptCaught {
+    static int x;
+    static String [] ar = {"Joe", "Matt", "Morgan", "Eddie", "Sarah", "Mike", "Emma", "Sadie", "Mark", "James"};
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        try{
+            System.out.println("Input an integer to get the corresponding name:");
+             x = in.nextInt();
+            System.out.println(ar[x-1]);
+        }
+        catch(IndexOutOfBoundsException e){
+            System.out.println("please re-input an integer between 1 and 10:");
+             x = in.nextInt();
+            System.out.println(ar[9]);
+        }
+    }
+}
---
+++ ExceptionLab/ExceptionLabChallenge.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class ExceptionLabChallenge {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        double x;
+        try{
+            System.out.println("Input a number to find the square root:");
+            x = in.nextDouble();
+            if (x<0){
+                x = 10/0;
+            }
+            System.out.println((Math.sqrt(x)));
+        }
+        catch(ArithmeticException e){
+            System.out.println("You cannot take the square root of a negative number. Please enter a new number: ");
+            x = in.nextDouble();
+            System.out.println(Math.sqrt(x));
+        }
+    }
+
+}
